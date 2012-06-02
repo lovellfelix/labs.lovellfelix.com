cabd1ec68e53e18082cd8d09e3da8a782236b7a1
Lovell Felix <lovell.felix@gmail.com>
2012-06-02 12:02:38 -0500

    created php file structure

---
+++ _inc/header.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?php echo $pagetitle; ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<!-- Le styles -->
+		<link href="assets/css/bootstrap.css" rel="stylesheet">
+		<link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="assets/css/docs.css" rel="stylesheet">
+		<link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
+
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+		<!-- Le fav and touch icons -->
+		<link rel="shortcut icon" href="assets/ico/favicon.ico">
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+	</head>
+
+	<body data-spy="scroll" data-target=".subnav" data-offset="50">
+
+		<!-- Navbar
+		================================================== -->
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+
+					<ul class="nav">
+						<li class="">
+							<a href="#">labs · Lovell Felix</a>
+						</li>
+
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+
+			<!-- Navigation list
+			================================================== -->
+			<div class="inner">
+				<div class="page-header jumbotron masthead">
+					<h1>labs</h1><small>
+						<blockquote>
+							Playground where prospective clients and users can play around with prototypes of some of my wild and crazy ideas and offer feedback.
+					</small></blockquote>
+				</div>
+				<div class="row">
+					<div class="span3 columns">
+
+						<ul class="nav nav-list">
+							<li>
+								<a href="#"> <i class="icon-home"></i> Home </a>
+							</li>
+							<li>
+								<a href="#"> <i class="icon-pencil"></i> Applications </a>
+							</li>
+							<li>
+								<a href="http://lovellfelix.com" target="_blank"> <i class="icon-user"></i> Resume </a>
+							</li>
+							<li>
+								<a href="http://lovellfelix.com" target="_blank"> <i class="icon-envelope"></i> Contact </a>
+							</li>
+							<li class="divider"></li>
+							<li>
+								<a href="#"> <i class="icon-question-sign"></i> Help </a>
+							</li>
+
+						</ul>
+
+						<br />
+						<br />
+						<blockquote>
+							<p>
+								"The beautiful thing about learning is no one can take it away from you"
+							</p>
+							<small>B. B. King</small>
+						</blockquote>
+
+					</div>
--- index.php
+++ index.php
@@ -1,295 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>labs · Lovell Felix Projects</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="assets/css/docs.css" rel="stylesheet">
-    <link href="assets/js/google-code-prettify/prettify.css" rel="stylesheet">
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="assets/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-  </head>
-
-  <body data-spy="scroll" data-target=".subnav" data-offset="50">
-
-
-  <!-- Navbar
-    ================================================== -->
-   	<div class="navbar navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					
-					<ul class="nav">
-						<li class="">
-							<a href="#">labs · Lovell Felix</a>
-						</li>
-						
-					</ul>
+<?php $pagetitle = "labs · Lovell Felix Projects"; ?>
+<?php include_once ('_inc/header.php'); ?>
+<!-- Carousel 
+================================================== -->
+<section id="carousel" class="">
+	<div class="span9 columns">
+		<div id="myCarousel" class="carousel slide">
+			<div class="carousel-inner">
+				<div class="item active">
+					<a href="http://www.natural-environment.com/places/milford_sound.php" target="_blank"> <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt=""> <div class="carousel-caption"></a>
+					<h4>First Thumbnail label</h4>
+					<p>
+						Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+					</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Second Thumbnail label</h4>
+					<p>
+						Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+					</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
+				<div class="carousel-caption">
+					<h4>Third Thumbnail label</h4>
+					<p>
+						Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+					</p>
 				</div>
 			</div>
 		</div>
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+	</div>
 
-    <div class="container">
+	</div>
+	</div>
 
-     <!-- Navigation list
-    ================================================== -->
-    <div class="inner">
-    	  <div class="page-header jumbotron masthead">
-					<h1>labs</h1> <small>
-						<blockquote>Playground where prospective clients and users can play around with prototypes of some of my wild and crazy ideas and offer feedback. </small></blockquote>
-				</div>
-				<div class="row">
-					<div class="span3 columns">
-					
-				<ul class="nav nav-list">
-							<li>
-								<a href="#"> <i class="icon-home"></i> Home </a>
-							</li>
-							<li>
-								<a href="#"> <i class="icon-pencil"></i> Applications </a>
-							</li>
-							<li>
-								<a href="http://lovellfelix.com" target="_blank"> <i class="icon-user"></i> Resume </a>
-							</li>
-							<li>
-								<a href="http://lovellfelix.com" target="_blank"> <i class="icon-envelope"></i> Contact </a>
-							</li>
-							<li class="divider"></li>
-							<li>
-								<a href="#"> <i class="icon-question-sign"></i> Help </a>
-							</li>
+</section>
 
-						</ul>
-						
-						<br />
-						<br />
-						<blockquote>
-							<p>
-								"The beautiful thing about learning is no one can take it away from you" 
-							</p>
-							<small>B. B. King</small>
-						</blockquote>
-											
-					</div>
- <!-- Carousel
-    ================================================== -->
-    <section id="carousel" class="">
-             <div class="span9 columns">
-          <div id="myCarousel" class="carousel slide">
-            <div class="carousel-inner">
-              <div class="item active"><a href="http://www.natural-environment.com/places/milford_sound.php" target="_blank">
-                <img src="assets/img/bootstrap-mdo-sfmoma-01.jpg" alt="">
-                <div class="carousel-caption"></a>
-                  <h4>First Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-              </div>
-              <div class="item">
-                <img src="assets/img/bootstrap-mdo-sfmoma-02.jpg" alt="">
-                <div class="carousel-caption">
-                  <h4>Second Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-              </div>
-              <div class="item">
-                <img src="assets/img/bootstrap-mdo-sfmoma-03.jpg" alt="">
-                <div class="carousel-caption">
-                  <h4>Third Thumbnail label</h4>
-                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </div>
-              </div>
-            </div>
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-          </div>
-                  
-                    
-        </div>
-      </div>
+<hr class="soften">
 
-    </section>
-
-
-<!-- Masthead
-================================================== -->
-<header class="jumbotron masthead">
-  <div class="inner">
-    
-  </div>
-
-  <!--<div class="bs-links">
-   
-    <ul class="quick-links">
-      <li>
-        <iframe class="github-btn" src="http://markdotto.github.com/github-buttons/github-btn.html?user=twitter&repo=bootstrap&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="112px" height="20px"></iframe>
-      </li>
-      <li>
-        <iframe class="github-btn" src="http://markdotto.github.com/github-buttons/github-btn.html?user=twitter&repo=bootstrap&type=fork&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="98px" height="20px"></iframe>
-      </li>
-      <li class="follow-btn">
-        <a href="https://twitter.com/twbootstrap" class="twitter-follow-button" data-link-color="#0069D6" data-show-count="true">Follow @twbootstrap</a>
-      </li>
-      <li class="tweet-btn">
-        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://twitter.github.com/bootstrap/" data-count="horizontal" data-via="twbootstrap" data-related="mdo:Creator of Twitter Bootstrap">Tweet</a>
-      </li>
-    </ul>
-  </div>-->
-</header>
-
-<!--<hr class="soften">
-
-<div class="marketing">
-  <h1>Designed for everyone, everywhere.</h1>
-  <p class="marketing-byline">Need reasons to love Bootstrap? Look no further.</p>
-  <div class="row">
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_042_group.png">
-      <h2>Built for and by nerds</h2>
-      <p>Like you, we love building awesome products on the web. We love it so much, we decided to help people just like us do it easier, better, and faster. Bootstrap is built for you.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_079_podium.png">
-      <h2>For all skill levels</h2>
-      <p>Bootstrap is designed to help people of all skill levels&mdash;designer or developer, huge nerd or early beginner. Use it as a complete kit or use to start something more complex.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_163_iphone.png">
-      <h2>Cross-everything</h2>
-      <p>Originally built with only modern browsers in mind, Bootstrap has evolved to include support for all major browsers (even IE7!) and, with Bootstrap 2, tablets and smartphones, too.</p>
-    </div>
-  </div>--><!--/row-->
-  <!--<div class="row">
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_155_show_thumbnails.png">
-      <h2>12-column grid</h2>
-      <p>Grid systems aren't everything, but having a durable and flexible one at the core of your work can make development much simpler. Use our built-in grid classes or roll your own.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_214_resize_small.png">
-      <h2>Responsive design</h2>
-      <p>With Bootstrap 2, we've gone fully responsive. Our components are scaled according to a range of resolutions and devices to provide a consistent experience, no matter what.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_266_book_open.png">
-      <h2>Styleguide docs</h2>
-      <p>Unlike other front-end toolkits, Bootstrap was designed first and foremost as a styleguide to document not only our features, but best practices and living, coded examples.</p>
-    </div>
-  </div>--><!--/row-->
-  <!--<div class="row">
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_082_roundabout.png">
-      <h2>Growing library</h2>
-      <p>Despite being only 10kb (gzipped), Bootstrap is one of the most complete front-end toolkits out there with dozens of fully functional components ready to be put to use.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/glyphicons/glyphicons_009_magic.png">
-      <h2>Custom jQuery plugins</h2>
-      <p>What good is an awesome design component without easy-to-use, proper, and extensible interactions? With Bootstrap, you get custom-built jQuery plugins to bring your projects to life.</p>
-    </div>
-    <div class="span4">
-      <img class="bs-icon" src="assets/img/less-small.png">
-      <h2>Built on LESS</h2>
-      <p>Where vanilla CSS falters, LESS excels. Variables, nesting, operations, and mixins in LESS makes coding CSS faster and more efficient with minimal overhead.</p>
-    </div>
-  </div>--><!--/row-->
-  <!--<div class="row">
-    <div class="span3">
-      <img class="small-bs-icon" src="assets/img/icon-html5.png">
-      <h3>HTML5</h3>
-      <p>Built to support new HTML5 elements and syntax.</p>
-    </div>
-    <div class="span3">
-      <img class="small-bs-icon" src="assets/img/icon-css3.png">
-      <h3>CSS3</h3>
-      <p>Progressively enhanced components for ultimate style.</p>
-    </div>
-    <div class="span3">
-      <img class="small-bs-icon" src="assets/img/icon-github.png">
-      <h3>Open-source</h3>
-      <p>Built for and maintained by the community via <a href="https://github.com">GitHub</a>.</p>
-    </div>
-    <div class="span3">
-      <img class="small-bs-icon" src="assets/img/icon-twitter.png">
-      <h3>Made at Twitter</h3>
-      <p>Brought to you by an experienced <a href="http://twitter.com/fat">engineer</a> and <a href="http://twitter.com/mdo">designer</a>.</p>
-    </div>
-  </div>--><!--/row-->
-
-  <hr class="soften">
-
-  <p class="marketing-byline">Graduated lab Projects</p>
-  <ul class="thumbnails example-sites">
-    <li class="span3">
-      <a class="thumbnail" href="http://soundready.fm/" target="_blank">
-        <img src="assets/img/example-sites/soundready.png" alt="SoundReady.fm">
-      </a>
-    </li>
-    <li class="span3">
-      <a class="thumbnail" href="http://kippt.com/" target="_blank">
-        <img src="assets/img/example-sites/kippt.png" alt="Kippt">
-      </a>
-    </li>
-    <li class="span3">
-      <a class="thumbnail" href="http://www.fleetio.com/" target="_blank">
-        <img src="assets/img/example-sites/fleetio.png" alt="Fleetio">
-      </a>
-    </li>
-    <li class="span3">
-      <a class="thumbnail" href="http://www.jshint.com/" target="_blank">
-        <img src="assets/img/example-sites/jshint.png" alt="JS Hint">
-      </a>
-    </li>
-  </ul>
+<p class="marketing-byline">
+	Graduated lab Projects
+</p>
+<ul class="thumbnails example-sites">
+	<li class="span3">
+		<a class="thumbnail" href="http://soundready.fm/" target="_blank"> <img src="assets/img/example-sites/soundready.png" alt="SoundReady.fm"> </a>
+	</li>
+	<li class="span3">
+		<a class="thumbnail" href="http://kippt.com/" target="_blank"> <img src="assets/img/example-sites/kippt.png" alt="Kippt"> </a>
+	</li>
+	<li class="span3">
+		<a class="thumbnail" href="http://www.fleetio.com/" target="_blank"> <img src="assets/img/example-sites/fleetio.png" alt="Fleetio"> </a>
+	</li>
+	<li class="span3">
+		<a class="thumbnail" href="http://www.jshint.com/" target="_blank"> <img src="assets/img/example-sites/jshint.png" alt="JS Hint"> </a>
+	</li>
+</ul>
 
 </div><!-- /.marketing -->
-
-     <!-- Footer
-      ================================================== -->
-      <footer class="footer">
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; <a href="http://lovellfelix.com" target="_bank">Lovell Felix</a>, 
-        	</footer>
-
-    </div><!-- /container -->
-
-
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/google-code-prettify/prettify.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-    <script src="assets/js/application.js"></script>
-
-
-  </body>
-</html>
+<?php include_once ('_inc/footer.php'); ?>
