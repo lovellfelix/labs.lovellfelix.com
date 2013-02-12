1fb5062760515f62c3b846977cc969bb98a450f2
Lovell Felix <lovell.felix@gmail.com>
2013-02-12 12:13:58 -0600

    Added Angularjs

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var labs.lovellfelix.comApp = angular.module('labs.lovellfelix.comApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+labs.lovellfelix.comApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
