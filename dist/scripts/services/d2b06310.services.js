d84360fe37fcab927a5917584f66ab3ed3468f3e
Lovell Felix <lovell2juicy@yahoo.com>
2013-04-15 11:28:00 -0500

    Directory cleanup

--- dist/scripts/services/d2b06310.services.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('Services', ['ngResource']).
-    factory('Phone', function($resource){
-  return $resource('data/:projectId.json', {}, {
-    query: {method:'GET', params:{projectId:'project'}, isArray:true}
-  });
-});
